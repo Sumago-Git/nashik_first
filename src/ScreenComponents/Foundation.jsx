4b36a8bf288c104c6d7f8c26d165d9f23a595502
devika15p <punddevika@gmail.com>
2024-10-19 12:33:49 +0530

    changes

--- src/ScreenComponents/Foundation.jsx
+++ src/ScreenComponents/Foundation.jsx
@@ -9,17 +9,15 @@ import Col from 'react-bootstrap/Col';
 const Foundation = () => {
   return (
     <>
-      <Container fluid className='body text-start mt-lg-5 pb-lg-5'>
-        <Row>
-          <Col lg={6} className='mt-lg-5 pt-lg-5 mt-4 m-md-0  p-0 ms-1 '>
+      <Container fluid className='body text-start py-5  '>
+        <Row className='d-flex align-items-center'>
+          <Col lg={7} className=''>
             <text className='Fheadline ms-lg-5' style={{ fontWeight: "650", color: "#c50c1c" }}>The Advantage
               Nashik Foundation.</text>
-            <Col className='mt-4'>
-              <p className=' mt-lg-3 mt-5 fpara1'><span style={{ color: "#c50c1c", fontWeight: "800" }}>Nashik</span> <span style={{ color: "#F96945", fontWeight: "800" }}> First</span>, an initiative of <span style={{ fontWeight: "800" }}>Advantage Nashik</span>
+              <p className='fpara1'><span style={{ color: "#c50c1c", fontWeight: "800" }}>Nashik</span> <span style={{ color: "#F96945", fontWeight: "800" }}> First</span>, an initiative of <span style={{ fontWeight: "800" }}>Advantage Nashik</span>
                  <span style={{ fontWeight: "800" }}> Foundation (ANF) </span>is a group of people working for
                 <span>improving quality of life of Citizens of Nashik.</span></p>
-            </Col>
-            <Col className=' fpara2 mt-4'>
+            <div className=' fpara2 '>
               We are a group of people who act as a catalyst for the growth of the city 
               and in many cases, becoming the primary factor responsible for the growth.
               Through independent assessment of different projects & various activities
@@ -30,12 +28,12 @@ const Foundation = () => {
               going to be really meaningful for the development of the city, we strives to
               be a foundation which acts through a systematic methodology to achieve
               specific goals.
-            </Col>
+            </div>
           </Col>
 
-          <Col lg={5} className='mt-lg-5 pt-5 p-0 justify-content-end'>
+          <Col lg={5} className='p-0 justify-content-end'>
 
-            <img src={group} className='group img-fluid  ms-lg-5 ' />
+            <img src={group} className='group img-fluid flex-end ' />
 
           </Col>
         </Row>
