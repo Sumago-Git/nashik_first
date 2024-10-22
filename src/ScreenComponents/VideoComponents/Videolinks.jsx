cce0c4bbd74401b944512518f977c59a7a145b23
shubham <shubham.kothavade09@gmail.com>
2024-10-22 14:23:57 +0530

    changes

--- src/ScreenComponents/VideoComponents/Videolinks.jsx
+++ src/ScreenComponents/VideoComponents/Videolinks.jsx
@@ -20,51 +20,34 @@ const Youtube = () => {
       <Container className='mt-lg-5'>
         <p className='vdoheadline pb-lg-4 mt-3'>Our Videos</p>
         <Row className="mx-auto">
-          
+
           {/* Video 1 */}
           <Col xs={12} md={6} className='mb-4'>
-            <div className="video-container" style={{ position: 'relative', paddingBottom: '56.25%', height: 0, overflow: 'hidden' }}>
-              {!video1Loaded ? (
-                <img
-                src={yt1}
-                alt="Custom Thumbnail"
-                style={{ width: '100%', cursor: 'pointer' }}
-                onClick={loadVideo1}
-              />
-              ) : (
-                <iframe
-                  style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
-                  src="https://www.youtube.com/embed/RmJXd5fviBA?autoplay=1"
-                  title="YouTube video player"
-                  frameBorder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                  allowFullScreen
-                />
-              )}
-            </div>
+          <iframe
+              className="w-100 rounded-3"
+              style={{ height: "250px" }}
+              src="https://www.youtube.com/embed/GCQnntHWj5s?si=orzUJ3jia6vl6Saz"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+            ></iframe>
           </Col>
-          
+
           {/* Video 2 */}
           <Col xs={12} md={6} className='mb-4'>
-            <div className="video-container" style={{ position: 'relative', paddingBottom: '56.25%', height: 0, overflow: 'hidden' }}>
-              {!video2Loaded ? (
-                <img
-                src={yt1}
-                alt="Custom Thumbnail"
-                style={{ width: '100%', cursor: 'pointer' }}
-                onClick={loadVideo2}
-              />
-              ) : (
-                <iframe
-                  style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
-                  src="https://www.youtube.com/embed/vmFT7ial_pM?autoplay=1"
-                  title="YouTube video player"
-                  frameBorder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                  allowFullScreen
-                />
-              )}
-            </div>
+            <iframe
+              className="w-100 rounded-3"
+              style={{ height: "250px" }}
+              src="https://www.youtube.com/embed/GCQnntHWj5s?si=orzUJ3jia6vl6Saz"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+            ></iframe>
+
           </Col>
 
           <Col className='pb-4'>
