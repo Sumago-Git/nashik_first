f70acd233a1cb0aed144edcdf638581b0bc554fe
shubham <shubham.kothavade09@gmail.com>
2024-11-13 10:57:27 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
   //   console.log("process.env.REACT_APP_API_BASE_URL", process.env.REACT_APP_API_BASE_URL)
     
   // }, [])
-  axios.defaults.baseURL = "http://127.0.0.1:8000/"
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL
   return (
     <div className="App">
 
